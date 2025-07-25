5903a1d6c96f0d6b6672f25fb40fbc545ae5d669
adiarra14 <a.diarra@sinigroup.net>
2025-07-25 23:26:10 +0200

    Add SDK server configuration and MaxvisionConfig class

---
+++ src/main/java/com/maxvision/demo/config/MaxvisionConfig.java
@@ -0,0 +1,14 @@
+package com.maxvision.demo.config;
+
+import com.maxvision.edge.gateway.sdk.config.LockSdkConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MaxvisionConfig {
+
+    @Bean
+    public LockSdkConfig lockSdkConfig() {
+        return new LockSdkConfig();
+    }
+} 
